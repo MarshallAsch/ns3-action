dec2e471c90cbcbbe085fa2d86623ec622869346
Marshall Asch <maasch@rogers.com>
2021-04-10 12:52:25 -0400

    updated the script to copy / build the code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# copy all of the files into the scratch folder
+cp -r "$GITHUB_WORKSPACE/*" /ns3/ns-allinone-${NS3_VERSION}/ns-${NS3_VERSION}/scratch/
 
-echo "$GITHUB_WORKSPACE"
-ls $GITHUB_WORKSPACE
 
+# test build the app
+./waf build
