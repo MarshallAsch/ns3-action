ba43c8a4661b57ed2b5e1b3d692b88f2af9c0ab5
Marshall Asch <maasch@rogers.com>
2021-04-10 19:53:31 -0400

    added an input for the simulation runner so it can run a sample test simulation

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 # copy all of the files into the scratch folder
-cp -r "$GITHUB_WORKSPACE/*" /ns3/ns-allinone-${NS3_VERSION}/ns-${NS3_VERSION}/scratch/
-
+cp -r "$GITHUB_WORKSPACE" /ns3/ns-allinone-${NS3_VERSION}/ns-${NS3_VERSION}/scratch/
+cd  /ns3/ns-allinone-${NS3_VERSION}/ns-${NS3_VERSION}
 
 # test build the app
 ./waf build
+
+
+# run a test simulation to ensure that it runs if a parameter is set
+if [[ -z "$INPUT_SIM_NAME" ]]
+then
+    echo "No simulation name given, not running a sample run"
+else
+    ./waf --run "scratch/workspace/$INPUT_SIM_NAME $INPUT_SIM_ARGS"
+fi
+
